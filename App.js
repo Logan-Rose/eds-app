3d07c9a10c96c716e1b4f9f7572074872d826ddf
Logan-Rose <logan.rose0329@gmail.com>
2021-08-23 17:02:20 -0400

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, Alert, TextInput, Image} from 'react-native';
+//<Image style={styles.logo} source = {require("./assets/logo.png")}/>
 export default function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>[Logo] {"\n"}</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        onChangeText={(email) => setEmail(email)}
+      />
+      <TextInput        
+        secureTextEntry={true}
+        style={styles.input}
+        placeholder="Password"
+        onChangeText={(password) => setPassword(password)}
+      />
+      <View style={{width:150}}>
+        <Button        
+          style={styles.button}
+          onPress={()=> Alert.alert('Email: '+ email +'\nPassword: ' + password)}
+          title="Sign in"
+          color="grey"
+          width="200"
+        />
+        <View style={styles.space} />
+        <Button        
+          style={styles.button}
+          onPress={()=> Alert.alert('Sign Up')}
+          title="Sign up"
+          color="grey"
+          padding="10"
+        />
+      </View>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +45,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#8f001a',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input:{
+    backgroundColor:"white",
+    lineHeight: 30,
+    marginBottom: 20,
+    padding: 10,
+    width: 200
+  },
+  button: {
+    marginTop:20,
+    backgroundColor:"green"
+  },
+  logo:{
+    width: 0.25,
+    height: 0.25
+  },
+  space:{
+    height: 10
+  }
 });
