2bde9a0b3472cf535463909ea7af13d7f5beb2ad
Logan-Rose <logan.rose0329@gmail.com>
2021-08-24 18:39:35 -0400

    fleshed out front end barebones structure

--- App.js
+++ App.js
@@ -1,8 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button, Alert, TextInput, Image} from 'react-native';
+import { NativeRouter, Route, Link } from "react-router-native";
+import { CheckBox } from 'react-native-elements'
 //<Image style={styles.logo} source = {require("./assets/logo.png")}/>
 export default function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  return (
+    <NativeRouter>
+    <View style={styles.container}>
+      <Route exact path="/" component={Home} />
+      <Route path="/signup" component={SignUp} />
+    </View>
+  </NativeRouter>
+  );
+}
+
+const Home = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   return (
@@ -10,7 +25,7 @@ export default function App() {
       <Text>[Logo] {"\n"}</Text>
       <TextInput
         style={styles.input}
-        placeholder="Username"
+        placeholder="Email"
         onChangeText={(email) => setEmail(email)}
       />
       <TextInput        
@@ -19,27 +34,79 @@ export default function App() {
         placeholder="Password"
         onChangeText={(password) => setPassword(password)}
       />
-      <View style={{width:150}}>
+      <View style={{width:150, backgroundColor:"green",  alignItems:'center'}}>
         <Button        
           style={styles.button}
           onPress={()=> Alert.alert('Email: '+ email +'\nPassword: ' + password)}
           title="Sign in"
           color="grey"
-          width="200"
         />
         <View style={styles.space} />
-        <Button        
-          style={styles.button}
-          onPress={()=> Alert.alert('Sign Up')}
-          title="Sign up"
-          color="grey"
-          padding="10"
-        />
+        <Link to="/signup">
+          <Text>Sign Up</Text>
+        </Link>
       </View>
       
       <StatusBar style="auto" />
     </View>
   );
+}
+
+const SignUp = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [agree, toggleAgree] = useState('');
+  return (
+    <View style={styles.container}>
+      <Text>[Sign Up] {"\n"}</Text>
+      <View style={styles.signUpInfo}>
+        <TextInput
+          style={styles.input}
+          placeholder="First Name"
+          onChangeText={(firstName) => setEmail(firstName)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Last Name"
+          onChangeText={(lastName) => setEmail(lastName)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          onChangeText={(email) => setEmail(email)}
+        />
+        <TextInput        
+          secureTextEntry={true}
+          style={styles.input}
+          placeholder="Password"
+          onChangeText={(password) => setPassword(password)}
+        />
+        <Text>I confirm that I have not experienced COVID-19 symptoms in the past 14 days</Text>
+        <CheckBox
+          checked={agree}
+          onPress={() => this.toggleAgree(!agree)}
+          title='I agree'
+        />
+      </View>
+      <View style={styles.space} />
+      <View style={{width:150}}>
+        <Button        
+          style={styles.button}
+          onPress={()=> Alert.alert('First Name:'+ firstName + ' Last Name:'+ lastName + '\nEmail: '+ email +'\nPassword: ' + password)}
+          title="Sign Up"
+          color="grey"
+          width="200"
+        />
+      </View>
+      <View style={styles.space} />
+      <Link to="/">        
+        <Text>Back</Text>
+      </Link>
+    </View>
+
+  )
 }
 
 const styles = StyleSheet.create({
@@ -51,6 +118,7 @@ const styles = StyleSheet.create({
   },
   input:{
     backgroundColor:"white",
+    borderColor: "black",
     lineHeight: 30,
     marginBottom: 20,
     padding: 10,
@@ -66,5 +134,11 @@ const styles = StyleSheet.create({
   },
   space:{
     height: 10
+  },
+  signUpInfo:{
+    alignSelf:'center',
+    margin: 100,
+    backgroundColor:"white",
+    padding:25
   }
 });
